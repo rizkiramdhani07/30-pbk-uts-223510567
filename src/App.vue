597b3c7234e9653ce08753bca6a5f256232798bd
rizkiramdhani07 <rzkyramdhani07@gmail.com>
2024-05-14 00:37:20 +0700

    tugas minggu 10

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,50 @@
 <template>
   <div class="todo-app">
-    <h1>"Daftar Kegiatan Anda"</h1>
-    <p>create by Muhammad Rizki Ramadhani</p>
+    <header>
+      <nav>
+        <ul>
+          <li @click="activeTab = 'todos'" :class="{ active: activeTab === 'todos' }">Todos</li>
+          <li @click="activeTab = 'posts'" :class="{ active: activeTab === 'posts' }">Post</li>
+        </ul>
+      </nav>
+    </header>
 
-    <div class="filter-buttons">
-      <button @click="filter = 'all'">Semua</button>
-      <button @click="filter = 'done'">Selesai</button>
-      <button @click="filter = 'pending'">Belum Selesai</button>
+    <div v-if="activeTab === 'todos'" class="todos">
+      <h1>Daftar Kegiatan Anda</h1>
+      <p class="author">create by Muhammad Rizki Ramadhani</p>
+
+      <div class="filter-buttons">
+        <button @click="filter = 'all'" :class="{ active: filter === 'all' }">Semua</button>
+        <button @click="filter = 'done'" :class="{ active: filter === 'done' }">Selesai</button>
+        <button @click="filter = 'pending'" :class="{ active: filter === 'pending' }">Belum Selesai</button>
+      </div>
+
+      <input type="text" v-model="newTodo" placeholder="Masukkan item kegiatan baru" />
+      <button @click="addTodo">Tambah</button>
+      <hr>
+      <ul>
+        <li v-for="(todo, index) in filteredTodos" :key="index" :class="{ done: todo.done }">
+          <input type="checkbox" v-model="todo.done" />
+          <span>{{ todo.text }}</span>
+          <button @click="removeTodo(index)">Hapus</button>
+          <hr>
+        </li>
+      </ul>
     </div>
 
-    <input type="text" v-model="newTodo" placeholder="Masukkan item kegiatan baru" />
-    <button @click="addTodo">Tambah</button>
-    <hr>
-    <ul>
-      <li v-for="(todo, index) in filteredTodos" :key="index" :class="{ done: todo.done }">
-        <input type="checkbox" v-model="todo.done" />
-        <span>{{ todo.text }}</span>
-        <button @click="removeTodo(index)">Hapus</button>
-        <hr>
-      </li>
-    </ul>
+    <div v-else-if="activeTab === 'posts'" class="posts">
+      <h1>Postingan User</h1>
+      <select v-model="selectedUser">
+        <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
+      </select>
+      <ul v-if="posts.length > 0">
+        <li v-for="post in filteredPosts" :key="post.id">
+          <h2>{{ post.title }}</h2>
+          <p class="post-body">{{ post.body }}</p>
+        </li>
+      </ul>
+      <p v-else>Tidak ada postingan untuk user yang dipilih.</p>
+    </div>
   </div>
 </template>
 
@@ -27,9 +52,13 @@
 export default {
   data() {
     return {
+      activeTab: 'todos',
       newTodo: '',
       todos: [],
-      filter: 'all', 
+      filter: 'all', // 'all', 'done', 'pending'
+      users: [],
+      posts: [],
+      selectedUser: null
     };
   },
   computed: {
@@ -42,8 +71,26 @@ export default {
         return this.todos.filter(todo => !todo.done);
       }
     },
+    filteredPosts() {
+      if (this.selectedUser) {
+        return this.posts.filter(post => post.userId === this.selectedUser);
+      } else {
+        return [];
+      }
+    }
+  },
+  async created() {
+    await this.fetchUsers();
   },
   methods: {
+    async fetchUsers() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      this.users = await response.json();
+    },
+    async fetchPosts() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      this.posts = await response.json();
+    },
     addTodo() {
       if (this.newTodo.trim()) {
         this.todos.push({
@@ -57,71 +104,12 @@ export default {
       this.todos.splice(index, 1);
     },
   },
+  watch: {
+    selectedUser(newVal) {
+      if (newVal) {
+        this.fetchPosts();
+      }
+    }
+  }
 };
 </script>
-
-<style scoped>
-.todo-app {
-  font-family: sans-serif;
-  
-}
-
-ul > li > button{
-  background-color: red;
-  color: white;
-}
-
-.todo-app input[type="text"] {
-  padding: 10px;
-  margin-bottom: 10px;
-}
-
-.todo-app button {
-  padding: 5px 10px;
-  cursor: pointer;
-}
-
-.todo-app ul {
-  list-style: none;
-  padding: 0;
-}
-
-.todo-app li {
-  margin-bottom: 10px;
-}
-
-.todo-app li input[type="checkbox"] {
-  margin-right: 10px;
-}
-
-.todo-app li span {
-  color: white;
-  text-decoration: none;
-}
-
-.todo-app li button {
-  float: right;
-  padding: 5px;
-  border: none;
-  cursor: pointer;
-}
-
-.todo-app li.done span {
-  text-decoration: line-through;
-}
-
-.filter-buttons {
-  margin-bottom: 10px;
-}
-
-.filter-buttons button {
-  padding: 5px 10px;
-  margin-right: 5px;
-  cursor: pointer;
-  border: 1px solid #ccc;
-}
-
-.filter-buttons button.active {
-  background-color: #eee;
-}
-</style>
